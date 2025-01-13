0d47067ff845b7fec9f4ae5998ee0f69819230dd
mohitsingh4716 <mohitsingh4716@gmail.com>
2025-01-13 17:20:15 +0530

    createTransaction_Initialize

---
+++ app/(main)/transaction/_components/transaction-form.jsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+const AddTransactionForm = () => {
+  return (
+    <div>TransactionForm</div>
+  )
+}
+
+export default AddTransactionForm
--- app/lib/schema.js
+++ app/lib/schema.js
@@ -7,3 +7,24 @@ export const accountShema= z.object({
     isDefault: z.boolean().default(false),
     
 })
+
+export const transactionSchema= z.object({
+    accountId: z.string().min(1, "Account is required"),
+    type: z.enum(["EXPENSE", "INCOME"]),
+    amount: z.string().min(1, "Amount is required"),
+    date: z.date({required_error: "Date is required"}),
+    description: z.string().optional(),
+    category: z.string().min(1, "Category is required"),
+    isRecurring: z.boolean().default(false),
+    recurringInterval: z.enum(["DAILY", "WEEKLY", "MONTHLY", "YEARLY"]).optional(),
+})
+.superRefine((data, ctx)=>{
+    if(data.isRecurring && !data.recurringInterval){
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Recurring interval is required for recurring transactions",
+            path: ["recurringInterval"],
+        });
+    }
+    
+})
