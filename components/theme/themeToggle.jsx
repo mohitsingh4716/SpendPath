f462d476970ec7eb9d67fb0b0c9e2a42dec96ed1
mohitsingh4716 <mohitsingh4716@gmail.com>
2025-01-18 13:24:50 +0530

    ThemeButtonUpdated

--- components/theme/themeToggle.jsx
+++ components/theme/themeToggle.jsx
@@ -7,29 +7,19 @@ import { Moon, Sun } from 'lucide-react'
 
 
 const ThemeToggle = () => {
-    const { setTheme } = useTheme()
+    const { setTheme, theme } = useTheme()
 
   return (
     <div>
                 <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="outline" size="icon">
-                      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <DropdownMenuTrigger>
+                    <Button variant="outline" size="icon"  onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+                      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
                       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                      <span className="sr-only">Toggle theme</span>
+                      <span className="sr-only">Set theme</span>
                     </Button>
                   </DropdownMenuTrigger>
-                  <DropdownMenuContent align="end">
-                    <DropdownMenuItem onClick={() => setTheme("light")}>
-                      Light
-                    </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => setTheme("dark")}>
-                      Dark
-                    </DropdownMenuItem>
-                    <DropdownMenuItem onClick={() => setTheme("system")}>
-                      System
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
+                  
               </DropdownMenu>
     </div>
   )
